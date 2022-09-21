aaf1f430ceace7b2f5420a505bd08eafa339c1c5
ketan <20bcs6449@cuchd.in>
2022-09-22 00:45:48 +0530

    added hero

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={`${styles.hero}`}>
+      <div
+        className={`${styles.hero_content} flex justify-content items-center direction-col`}
+      >
+        <div className={styles.logo}>Apna Helper...</div>
+        <div className={`${styles.button} `}>
+          <button>Post your job</button>
+          <button className={styles.right_button}>Become a helper</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from '../../components/Hero/Hero'
 
 const Home = () => {
     return (
         <>
+<<<<<<< HEAD
             
+=======
+            <div>
+                <Hero/>
+            </div>
+>>>>>>> 6f3bee4 (added hero)
         </>
     );
 }
