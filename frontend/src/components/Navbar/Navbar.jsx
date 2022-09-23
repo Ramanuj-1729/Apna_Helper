08691464bbf1ff4a1dfd6e448e72d86c595fabb3
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2022-09-23 17:15:18 +0530

    update navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Navbar.module.css";
 import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
+  const [shadow, setShadow] = useState(false);
+
+  window.onscroll = function () {
+    if (window.scrollY > 60) {
+      setShadow(true);
+    }
+    else {
+      setShadow(false);
+    }
+  }
   return (
-    <div className={`${styles.header} flex items-center justify-between `} >
-      <div className={`${styles.header_left} `}>
-        <h1 className={styles.logo}>Apna Helper</h1>
+    <div className={`${styles.navbar} ${shadow === true ? styles.drope_shadow : ""} flex items-center justify-between `} >
+      <div className={`${styles.navbar_left} `}>
+        <NavLink to="/"> <h1 className={styles.logo}>Apna Helper</h1> </NavLink>
       </div>
 
-      <div className={`${styles.header_right} `} >
-        <NavLink to="#">Home</NavLink>
-        <NavLink to="#">About</NavLink>
-        <NavLink to="#">Contact</NavLink>
-        <NavLink to="#">Log In</NavLink>
+      <div className={`${styles.navbar_right} flex items-center justify-between `} >
+        <div className="menu">
+          <NavLink to="#">Home</NavLink>
+          <NavLink to="#">About</NavLink>
+          <NavLink to="#">Contact</NavLink>
+        </div>
 
-        <button>Sign Up</button>
+        <div className={styles.auth}>
+          <button className={`${styles.login} ${styles.navBtn}`}>Log In</button>
+          <button className={`${styles.signup} ${styles.navBtn}`}>Sign Up</button>
+        </div>
       </div>
     </div>
   );
