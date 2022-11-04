f5908fc11f63a36551d32f956494163f52758fc9
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2022-11-05 02:06:42 +0530

    add input in mobileNumber

--- frontend/src/pages/Registration/RegistrationSteps/BuildProfile/BuildProfileSteps/MobileNumber/MobileNumber.jsx
+++ frontend/src/pages/Registration/RegistrationSteps/BuildProfile/BuildProfileSteps/MobileNumber/MobileNumber.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import PrimaryButton from '../../../../../../components/shared/PrimaryButton/PrimaryButton'
 import styles from './MobileNumber.module.css'
 import { useNavigate } from "react-router-dom";
+import Input from '../../../../../../components/shared/Input/Input';
 
 const MobileNumber = () => {
-        const Navigation = useNavigate();
-        const step3Navigation = () => {
-            Navigation("/register/build-profile/verification");
-        
-    }
+  const Navigation = useNavigate();
+  const step3Navigation = () => {
+    Navigation("/register/build-profile/verification");
+
+  }
   return (
     <>
-    <div className='flex direction-col justify-center items-center'>
+      <div className='flex direction-col justify-center items-center'>
         <div className={styles.head}>
-                  <h3>Enter your phone number</h3>
-
+          <h3>Enter phone number</h3>
         </div>
-        <span>select your country and phone number</span>
-              <PrimaryButton content="Generate OTP" backgroundColor="#425E5E" padding="6px 80px"
-                  boxShadow="0px 8px 25px 2px rgba(66, 94, 94, 0.25)"
-                  onClick={step3Navigation}
-
-              ></PrimaryButton>
-    </div>
+        <span>select your country & phone number</span>
+        <Input name="phone-number" type="tel" iconClasses="fa-solid fa-phone" label="Phone Number" />
+        <PrimaryButton content="Generate OTP" backgroundColor="#425E5E" padding="6px 80px"
+          boxShadow="0px 8px 25px 2px rgba(66, 94, 94, 0.25)"
+          onClick={step3Navigation}>
+        </PrimaryButton>
+      </div>
     </>
   )
 }
--- frontend/src/pages/Registration/RegistrationSteps/BuildProfile/BuildProfileSteps/Verification/Verification.jsx
+++ frontend/src/pages/Registration/RegistrationSteps/BuildProfile/BuildProfileSteps/Verification/Verification.jsx
@@ -16,24 +16,23 @@ const Verification = () => {
                 <div className={`${styles.head} flex direction-col justify-center items-center`}>
                     <h3>OTP verification</h3>
                     <span>Enter the OTP sent to +91 2332323232</span>
-
                 </div>
                 <div className={`${styles.input} flex  justiyf-center items-center`}>
-                    <input type="number" />
-                    <input type="number" />
-                    <input type="number" />
-                    <input type="number" />
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" />
                 </div>
                 <div className='flex'>
                     <span>Didnt recieve OTP ?</span>
                     <div className={styles.color}>
                         <span>RESEND</span>
-                    </div>        </div>
+                    </div>
+                </div>
                 <PrimaryButton content="Verify" backgroundColor="#425E5E" padding="6px 80px"
                     boxShadow="0px 8px 25px 2px rgba(66, 94, 94, 0.25)"
-                  onClick={step4Navigation}
-
-                ></PrimaryButton>
+                    onClick={step4Navigation}>
+                </PrimaryButton>
             </div>
         </>
     )
